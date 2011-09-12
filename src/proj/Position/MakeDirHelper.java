5b54e30f36013b4072c58dae6bdecc11b5ab2ad4
T. M.L <tsai.danny602@gmail.com>
2011-09-12 15:05:17 +0800

    VER 0.8 (non onDraw)

--- src/proj/Position/MakeDirHelper.java
+++ src/proj/Position/MakeDirHelper.java
@@ -7,7 +7,8 @@ import android.os.Environment;
 public class MakeDirHelper {
 	public void make(String filePath, String fileName) {
 		File SDCardRoot = Environment.getExternalStorageDirectory();
-		File makeFilePath = new File(SDCardRoot + filePath + fileName);
+		File makeFilePath = new File(SDCardRoot + "/" + filePath + "/"
+				+ fileName);
 
 		makeFilePath.mkdir();
 	}
--- src/proj/Position/PositionActivity.java
+++ src/proj/Position/PositionActivity.java
@@ -2,12 +2,23 @@ package proj.Position;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import ntu.com.google.zxing.client.android.R;
+import android.R.string;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
@@ -23,6 +34,7 @@ import android.text.style.SuperscriptSpan;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -39,11 +51,17 @@ public class PositionActivity extends Activity {
 	private String title;
 
 	private Thread thread;
-	ProgressDialog aa;
-	
+	private ProgressDialog waitDownDialog;
+
 	// public enum mapKey {
 	// mapID, mapVer, title, map
 	// }
+
+	// Use for download handler message code
+	private interface messageCode {
+		public static final int DOWNLOAD_OK = 0;
+		public static final int DOWNLOAD_FAILED = 1;
+	}
 
 	/** Called when the activity is first created. */
 	@Override
@@ -83,28 +101,6 @@ public class PositionActivity extends Activity {
 		});
 	}
 
-	private void showImage() {
-
-		// Enable to zoom in/out
-
-		mapView.getSettings().setBuiltInZoomControls(true);
-		// Enable to zoom in/out by double tap
-		mapView.getSettings().setUseWideViewPort(true);
-
-		// Bind the image path
-		// String data =
-		// "<img src = \"file:///android_res/drawable/abc.png\" />";
-		// String data =
-		// "<img src = \"http:///indoorposition.appspot.com/image/map.jpg\" />";
-
-		String data = "<img src = \"file:///sdcard/somefile.jpg\" />";
-
-		final String mimeType = "text/html";
-		final String encoding = "utf-8";
-		mapView.loadDataWithBaseURL("about:blank", data, mimeType, encoding, "");
-
-	}
-
 	private void startScan() {
 		Intent intent = new Intent("ntu.com.google.zxing.client.android.SCAN");
 		intent.setPackage("ntu.com.google.zxing.client.android");
@@ -114,11 +110,12 @@ public class PositionActivity extends Activity {
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		
+
 		if (requestCode == 0) {
 			if (resultCode == RESULT_OK) {
 				String contents = intent.getStringExtra("SCAN_RESULT");
 				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+				Boolean isNeedUpdate;
 
 				try {
 					String[] contentArray = contents.split("\\?");
@@ -140,33 +137,68 @@ public class PositionActivity extends Activity {
 					pointTitle.setText(title);
 
 					// Check whether it need to download Json file
-					checkMapVerUpdate(mapID, mapVer);
-					
-					//aa = new AlertDialog.Builder(PositionActivity.this).create();
-					aa = new ProgressDialog(PositionActivity.this);
-					aa.setTitle("Downloading...");
-					aa.setMessage("Please wait");
-					aa.show();
-					// Start download Json file if need
-					thread = new Thread(new Runnable() {
-						// @Override
-						public void run() {
-							// super.run();
-							Log.e("test", "yo");
-							downloadMapJson(mapID);
-							// Message m = new Message();
-							// // 定義 Message的代號，handler才知道這個號碼是不是自己該處理的。
-							// m.what = MEG_INVALIDATE;
-							// handler.sendMessage(m);
-							handler.sendEmptyMessage(100);
-						}
-					});
+					isNeedUpdate = !checkMapVerUpdate(mapID, mapVer);
 
-					thread.start();
-					// downloadMapJson(mapID);
+					// Need to Update
+					if (isNeedUpdate) {
+						waitDownDialog = new ProgressDialog(
+								PositionActivity.this);
+						waitDownDialog.setTitle("Downloading...");
+						waitDownDialog.setMessage("Please wait!");
+						waitDownDialog.show();
+						// Start download Json file if need
+						thread = new Thread(new Runnable() {
+							// @Override
+							public void run() {
+								// super.run();
+								Log.e("test", "yo");
+
+								try {
+									// Test URL (real = jsonURL)
+									downloadMapJson(mapID,
+											"http://dl.dropbox.com/u/22034772/example.json");
+
+									// Test only download map img
+									File SD = Environment
+											.getExternalStorageDirectory();
+									File test = new File(SD
+											+ "/Position/last.json");
+
+									FileReader in = new FileReader(test);
+									BufferedReader stdin = new BufferedReader(
+											in);
+
+									String jsonString = "";
+									String jsonString1 = null;
+									while (((jsonString1 = stdin.readLine()) != null)) {
+										jsonString = jsonString + jsonString1;
+									}
+									in.close();
+
+									JSONObject jsonObj = new JSONObject(
+											jsonString);
+									String map = jsonObj.getString("map");
+									DownloadHelper downloader = new DownloadHelper();
+									downloader.downFile(map, Global.MapDirRoot
+											+ "/" + mapID, "map.jpg");
+
+									handler.sendEmptyMessage(messageCode.DOWNLOAD_OK);
+								} catch (MalformedURLException e) {
+									e.printStackTrace();
+								} catch (IOException e) {
+									e.printStackTrace();
+									handler.sendEmptyMessage(messageCode.DOWNLOAD_FAILED);
+								} catch (JSONException e) {
+									e.printStackTrace();
+								}
+							}
+						});
+						thread.start();
+					} else {
+						showMapData(mapID);
+					}
 
 				} catch (Exception e) {
-					// TODO: handle exception
 					// Handle when scan QR code which is not our form
 					pointTitle.setText("QR code error!");
 				}
@@ -177,10 +209,11 @@ public class PositionActivity extends Activity {
 
 				// Show AlertDialog for temporarily handle (Need to show last
 				// map)
-				Builder gg = new AlertDialog.Builder(PositionActivity.this);
-				gg.setMessage("Scan canceled");
-				gg.setTitle("Scan canceled").setPositiveButton("OK",
-						new DialogInterface.OnClickListener() {
+				Builder scanCancelDialog = new AlertDialog.Builder(
+						PositionActivity.this);
+				scanCancelDialog.setMessage("Scan canceled");
+				scanCancelDialog.setTitle("Scan canceled").setPositiveButton(
+						"OK", new DialogInterface.OnClickListener() {
 
 							@Override
 							public void onClick(DialogInterface dialog,
@@ -189,7 +222,11 @@ public class PositionActivity extends Activity {
 
 							}
 						});
-				gg.show();
+				scanCancelDialog.show();
+
+				// Show last map data
+				showLastMapData();
+
 			}
 		}
 	}
@@ -216,53 +253,159 @@ public class PositionActivity extends Activity {
 		return looker.look(Global.MapDirRoot, mapID);
 	}
 
-	private void downloadMapJson(String mapId) {
+	private void downloadMapJson(String mapId, String downHttpUrl)
+			throws IOException {
 		// Download the JSON file
 
 		DownloadHelper downloader = new DownloadHelper();
-		LookHelper looker = new LookHelper();
-		Boolean isExistent;
 
 		File SDCardRoot = Environment.getExternalStorageDirectory();
 		MakeDirHelper def = new MakeDirHelper();
 		def.make(Global.MapDirRoot, mapId);
 
-		try {
-			downloader.downFile(
-					"http://dl.dropbox.com/u/22034772/example.json",
-					Global.MapDirRoot + "/" + mapId, mapId + ".json");
+		downloader.downFile(downHttpUrl, Global.MapDirRoot + "/" + mapId, mapId
+				+ ".json");
 
-			File abc = new File(SDCardRoot + "/" + Global.MapDirRoot + "/"
-					+ mapId + "/" + mapId + ".json");
-			File dstFile = new File(SDCardRoot + "/" + Global.MapDirRoot + "/"
-					+ "last.json");
+		// Copy last downloaded JSON file in to map dir root
+		File abc = new File(SDCardRoot + "/" + Global.MapDirRoot + "/" + mapId
+				+ "/" + mapId + ".json");
+		File dstFile = new File(SDCardRoot + "/" + Global.MapDirRoot + "/"
+				+ "last.json");
 
-			BufferedInputStream in = new BufferedInputStream(
-					new FileInputStream(abc));
-			BufferedOutputStream out = new BufferedOutputStream(
-					new FileOutputStream(dstFile));
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(
+				abc));
+		BufferedOutputStream out = new BufferedOutputStream(
+				new FileOutputStream(dstFile));
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			// Handle when download failed
-			e.printStackTrace();
+		byte[] tmp = new byte[1024];
+
+		while (in.read(tmp) != -1) {
+			out.write(tmp);
 		}
+		in.close();
+		out.close();
+
 	}
 
 	private Boolean checkMapVerUpdate(String mapId, String mapVer) {
 		// Return False when it need to be updated
-		return true;
+		return false;
+	}
+
+	private Boolean checkOldMapData(String MapId) {
+		// Return true if there is old map data
+		LookHelper look = new LookHelper();
+		return look.look(Global.MapDirRoot, MapId);
+	}
+
+	private void showMapData(String mapID) {
+
+		// Enable to zoom in/out
+
+		mapView.getSettings().setBuiltInZoomControls(true);
+		// Enable to zoom in/out by double tap
+		mapView.getSettings().setUseWideViewPort(true);
+
+		// Bind the image path
+		// String data =
+		// "<img src = \"file:///android_res/drawable/abc.png\" />";
+		// String data =
+		// "<img src = \"http:///indoorposition.appspot.com/image/map.jpg\" />";
+
+		// String data = "<img src = \"file:///sdcard/somefile.jpg\" />";
+		String data = "<img src = \"file:///sdcard/" + Global.MapDirName + "/"
+				+ mapID + "/" + "map.jpg" + "\" />";
+
+		final String mimeType = "text/html";
+		final String encoding = "utf-8";
+
+		Global.x = 500;
+		mapView.invalidate();
+		mapView.loadDataWithBaseURL("about:blank", data, mimeType, encoding, "");
+
+	}
+
+	private void showLastMapData() {
+		// Show last Map data if it exists
+
+		File SDCardRoot = Environment.getExternalStorageDirectory();
+		File lastJson = new File(SDCardRoot + "/" + "Position" + "/"
+				+ "last.json");
+		FileReader in;
+
+		try {
+			in = new FileReader(lastJson);
+			BufferedReader stdin = new BufferedReader(in);
+
+			String jsonString = "";
+			String jsonString1 = null;
+			while (((jsonString1 = stdin.readLine()) != null)) {
+				jsonString = jsonString + jsonString1;
+			}
+			in.close();
+
+			JSONObject jsonObj = new JSONObject(jsonString);
+			String mapId = jsonObj.getString("mapID");
+
+			showMapData(mapId);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			pointTitle.setText("No last map data!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			pointTitle.setText("No last map data!");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			pointTitle.setText("No last map data!");
+		}
+
+		pointTitle.setText("Last map data!");
+
 	}
 
 	private Handler handler = new Handler() {
+		Boolean isOldMapData;
 
 		@Override
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
-			case 100:
-				aa.dismiss();
+			case messageCode.DOWNLOAD_OK:
+				// Show map data when download finished
+				showMapData(mapID);
+				waitDownDialog.dismiss();
+
+				break;
+			case messageCode.DOWNLOAD_FAILED:
+				// Show failed dialog when download failed
+				waitDownDialog.dismiss();
+				Builder gg = new AlertDialog.Builder(PositionActivity.this);
+				gg.setMessage("Download failed!");
+				gg.setTitle("Download failed!").setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+
+							}
+						});
+				gg.show();
 				break;
 			}
+
+			// Show old map data if it exist otherwise show last map
+			isOldMapData = checkOldMapData(mapID);
+			if (isOldMapData) {
+				showMapData(mapID);
+			} else {
+				showLastMapData();
+			}
+
+			super.handleMessage(msg);
 		}
 	};
 
