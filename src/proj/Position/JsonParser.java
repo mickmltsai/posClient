706ccf2bafc45c4408d27a16c0975df207f8e0c7
T. M.L <Danny@T-MLteki-MacBook-3.local>
2011-09-16 18:21:21 +0800

    Add JsonParser

--- src/proj/Position/JsonParser.java
+++ src/proj/Position/JsonParser.java
@@ -1,75 +1,51 @@
-//package proj.Position;
-//
-//
-//
-//import java.io.File;
-//
-//import android.os.Environment;
-//
-//import proj.Position.PositionActivity.mapKey;
-//
-//public class JsonParser {
-//	//Do not use this now
-//	File SDCardRoot = Environment.getExternalStorageDirectory();
-//
-//	public String getkMapValue(mapKey key) {
-//		// Get last download Json file data
-//
-//		switch (key) {
-//		// mapID,mapVer,title,map
-//		case mapID:
-//
-//			break;
-//		case mapVer:
-//
-//			break;
-//		case title:
-//
-//			break;
-//		case map:
-//
-//			break;
-//
-//		default:
-//			break;
-//		}
-//
-//		return null;
-//
-//	}
-//
-//	public String getkMapValue(mapKey key, String mapId) {
-//
-//		switch (key) {
-//		// mapID,mapVer,title,map
-//		case mapID:
-//
-//			break;
-//		case mapVer:
-//
-//			break;
-//		case title:
-//
-//			break;
-//		case map:
-//
-//			break;
-//
-//		default:
-//			break;
-//		}
-//
-//		return null;
-//
-//	}
-//
-//	public String[] getPointId(String mapId) {
-//		return null;
-//
-//	}
-//
-//	public String getPointValue(String mapId, String pointID, String key) {
-//		return null;
-//
-//	}
-//}
+package proj.Position;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import org.json.JSONObject;
+
+import android.os.Environment;
+
+public class JsonParser {
+
+	static File SDCardRoot = Environment.getExternalStorageDirectory();
+	static String oldJsonPath = null;
+
+	public static String getJsonRespon(String jsonPath) {
+		
+		if (jsonPath.equals(jsonPath)) {
+			
+			try {
+				
+		
+			File SD = Environment.getExternalStorageDirectory();
+			File test = new File(SD + "/Position/" + Global.MapId + "/"
+					+ Global.MapId + ".json");
+
+			FileReader in = new FileReader(test);
+			BufferedReader stdin = new BufferedReader(in);
+
+			String jsonString = "";
+			String jsonString1 = null;
+			while (((jsonString1 = stdin.readLine()) != null)) {
+				jsonString = jsonString + jsonString1;
+			}
+			in.close();
+
+			JSONObject jsonObj = new JSONObject(jsonString);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+			
+			return null;
+		} else {
+
+			return oldJsonPath;
+		}
+
+	}
+
+}
