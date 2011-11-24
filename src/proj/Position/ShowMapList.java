33b77b3fbba115cf9512ceef53e0854ffc131d53
T. M.L <tsai.danny602@gmail.com>
2011-11-25 02:12:06 +0800

    Fix maplist empty error

--- src/proj/Position/ShowMapList.java
+++ src/proj/Position/ShowMapList.java
@@ -7,6 +7,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import ntu.com.google.zxing.client.android.R;
+import android.R.integer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -47,90 +48,71 @@ public class ShowMapList extends Activity {
 	}
 
 	private void listMap() {
-		ArrayList<String> mStrings = new ArrayList<String>();
-
-		mapIdMapping = new ArrayList<String>();
 
 		File rootDir = new File(Global.SDPathRoot + "/" + Global.MapDirName + "/");
 		fileList = rootDir.listFiles();
-
-		// Adapt map's title
+		int count = 0;
 		for (int j = 0; j < fileList.length; j++) {
 			if (fileList[j].isDirectory()) {
-				try {
-					jsonObj = new JSONObject(JsonParser.getJsonRespon(Global.SDPathRoot + "/" + Global.MapDirName + "/" + fileList[j].getName() + "/" + fileList[j].getName() + ".json"));
-
-					if (jsonObj.getString("title") == null || jsonObj.getString("title").equals("")) {
-						mStrings.add("無地圖名稱");
-					} else {
-						mStrings.add(jsonObj.getString("title"));
-					}
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				mapIdMapping.add(fileList[j].getName());
+				count++;
 			}
 
 		}
 
-		list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrings));
-		// set filter
-		list.setTextFilterEnabled(true);
+		if (count != 0) {
+			ArrayList<String> mStrings = new ArrayList<String>();
 
-		// Handle after click map item
-		list.setOnItemClickListener(new OnItemClickListener() {
+			mapIdMapping = new ArrayList<String>();
 
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-				try {
-					jsonObj = new JSONObject(JsonParser.getJsonRespon(Global.SDPathRoot + "/" + Global.MapDirName + "/" + mapIdMapping.get(arg2) + "/" + mapIdMapping.get(arg2) + ".json"));
-					Global.MapTitle = jsonObj.getString("title");
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			// Adapt map's title
+			for (int j = 0; j < fileList.length; j++) {
+				if (fileList[j].isDirectory()) {
+					try {
+						jsonObj = new JSONObject(JsonParser.getJsonRespon(Global.SDPathRoot + "/" + Global.MapDirName + "/" + fileList[j].getName() + "/" + fileList[j].getName() + ".json"));
+
+						if (jsonObj.getString("title") == null || jsonObj.getString("title").equals("")) {
+							mStrings.add("無地圖名稱");
+						} else {
+							mStrings.add(jsonObj.getString("title"));
+						}
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					mapIdMapping.add(fileList[j].getName());
 				}
-				Global.MapId = mapIdMapping.get(arg2);
-				Global.PointId = null;
-				Global.PointTitle = null;
 
-				Intent intent = new Intent();
-				setResult(RESULT_OK, intent);
-
-				// ↓ Already do in PositionActivity showMapData()↓
-				// Copy last downloaded JSON file in to map dir root
-
-				// File srcFile = new File(Global.SDPathRoot + "/" +
-				// Global.MapDirName + "/" + mapIdMapping.get(arg2) + "/" +
-				// mapIdMapping.get(arg2) + ".json");
-				// File dstFile = new File(Global.SDPathRoot + "/" +
-				// Global.MapDirName + "/" + "last.json");
-				//
-				// BufferedInputStream in;
-				// try {
-				// in = new BufferedInputStream(new FileInputStream(srcFile));
-				//
-				// BufferedOutputStream out = new BufferedOutputStream(new
-				// FileOutputStream(dstFile));
-				//
-				// byte[] tmp = new byte[1024];
-				//
-				// while (in.read(tmp) != -1) {
-				// out.write(tmp);
-				// }
-				// in.close();
-				// out.close();
-				// } catch (FileNotFoundException e) {
-				// // TODO Auto-generated catch block
-				// e.printStackTrace();
-				// } catch (IOException e) {
-				// // TODO Auto-generated catch block
-				// e.printStackTrace();
-				// }
-
-				finish();
 			}
-		});
+
+			list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrings));
+			// set filter
+			list.setTextFilterEnabled(true);
+
+			// Handle after click map item
+			list.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+					try {
+						jsonObj = new JSONObject(JsonParser.getJsonRespon(Global.SDPathRoot + "/" + Global.MapDirName + "/" + mapIdMapping.get(arg2) + "/" + mapIdMapping.get(arg2) + ".json"));
+						Global.MapTitle = jsonObj.getString("title");
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					Global.MapId = mapIdMapping.get(arg2);
+					Global.PointId = null;
+					Global.PointTitle = null;
+
+					Intent intent = new Intent();
+					setResult(RESULT_OK, intent);
+
+					finish();
+				}
+			});
+		}else {
+			title.setText("無任何地圖");
+		}
 
 	}
 }
